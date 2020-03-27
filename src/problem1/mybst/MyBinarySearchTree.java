e01026792194e7b83eaa633bdc25bb78a8749926
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-26 22:26:50 -0700

    inorder ,postorder and preorder traversal method is created

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -38,5 +38,61 @@ public class MyBinarySearchTree {
         }
     }
 
+    private void printPreorder(TreeNode node) {
+        if (node == null)
+            return;
+
+        /* first print data of node */
+        System.out.print(node.getData() + " ");
+
+        /* then recur on left sutree */
+        printPreorder(node.getLeft());
+
+        /* now recur on right subtree */
+        printPreorder(node.getRight());
+    }
+
+
+    private void printPostorder(TreeNode node) {
+        if (node == null)
+            return;
+
+        // first recur on left subtree
+        printPostorder(node.getLeft());
+
+        // then recur on right subtree
+        printPostorder(node.getRight());
+
+        // now deal with the node
+        System.out.print(node.getData() + " ");
+    }
+
+    private void printInorder(TreeNode node) {
+        if (node == null)
+            return;
+
+        /* first recur on left child */
+        printInorder(node.getLeft());
+
+        /* then print the data of node */
+        System.out.print(node.getData() + " ");
+
+        /* now recur on right child */
+        printInorder(node.getRight());
+    }
+
+    // wrapper over recursive funtion
+    public void printPreorder() {
+        printPreorder(this.root);
+    }
+
+    public void printPostorder() {
+        printPostorder(this.root);
+    }
+
+    public void printInorder() {
+        printInorder(root);
+    }
+
 
 }
