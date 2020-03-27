f01def81a1d55e33b4aacdf64c71c6e5f8897dca
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-27 09:59:22 -0700

    getter and setter for fields of QNode class

--- src/problem4/node/QNode.java
+++ src/problem4/node/QNode.java
@@ -8,4 +8,20 @@ public class QNode {
         this.data = data;
         this.next = null;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public QNode getNext() {
+        return next;
+    }
+
+    public void setNext(QNode next) {
+        this.next = next;
+    }
 }
