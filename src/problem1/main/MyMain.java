fe3f0459dc05ab24a1121145ff8b81b6d8194dce
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-27 00:09:44 -0700

    driver class calling

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,26 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree bt = new MyBinarySearchTree();
+        bt.insert(50);
+        bt.insert(35);
+        bt.insert(65);
+        bt.insert(40);
+        bt.insert(30);
+        bt.insert(60);
+        bt.insert(64);
+        bt.insert(62);
+
+
+        bt.printPreorder();
+        System.out.println();
+        bt.printPostorder();
+        System.out.println();
+        bt.printLeftChild();
     }
+
+}
