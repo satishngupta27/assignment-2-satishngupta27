203716efa4783f58a35c9558df2a476c0a1cf89b
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-27 02:04:36 -0700

    created fields of node class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,27 @@
 package problem3.node;
 
 public class Node {
+    private Object obj;
+    private Node next;
+
+    public Node(Object obj) {
+        this.obj = obj;
+        this.next = null;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
