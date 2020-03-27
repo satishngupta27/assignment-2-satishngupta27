20b6953433f9f167748ef4ea617f1c241459d506
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-27 09:56:35 -0700

    fields of QNode class created

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,4 +8,6 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+
+
 }
---
+++ src/problem4/node/QNode.java
@@ -0,0 +1,6 @@
+package problem4.node;
+
+public class QNode {
+    int data;
+    QNode next;
+}
