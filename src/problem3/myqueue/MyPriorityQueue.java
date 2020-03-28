41fe4b2987ac585f53c044ce6b69ac2c05d312f8
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-28 06:29:41 -0700

    calling code

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -1,57 +1,73 @@
+package problem3.myqueue;
+
+import problem3.node.Node;
+import problem3.student.Student;
+
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
  *  Time: 7:15 PM
  */
-package problem3.myqueue;
-
-import problem3.node.Node;
-
 public class MyPriorityQueue {
-
-    Node front, rear;
+    private Node rear;
+    private Node front;
+    private int size;
 
     public MyPriorityQueue() {
-        this.front = this.rear = null;
+        rear = null;
+        front = null;
+        size = 0;
     }
 
-    // Method to add an key to the queue.
-    public void enqueue(Object obj) {
+    public void enqueue(Student data) {
+        Node node = new Node(data);
+        if (front == null) {
+            front = node;
+            rear = node;
+            size++;
+        } else {
+            Node temp = front;
+            int k = node.compareTo(temp.getData());
+            if (k > 0) {
+                node.setNext(temp);
+                front = node;
+                size++;
 
-        // Create a new LL node
-        Node temp = new Node(obj);
+            } else if (k <= 0) {
+                if (node.getData().getRollNo() > rear.getData().getRollNo()) {
+                    rear.setNext(node);
+                    rear = node;
+                    size++;
+                } else {
+                    Node first = front;
+                    while (first.getNext() != null) {
+                        if (node.getData().getRollNo() < (first.getNext()).getData().getRollNo() && node.getData().getRollNo() > first.getData().getRollNo()) {
+                            node.setNext(first.getNext());
+                            first.setNext(node);
+                            size++;
+                            break;
+                        }
+                    }
+                }
+            }
 
-        // If queue is empty, then new node is front and rear both
-        if (this.rear == null) {
-            this.front = this.rear = temp;
-            return;
+
         }
-
-        // Add the new node at the end of queue and change rear
-        this.rear.setNext(temp);
-        this.rear = temp;
     }
 
-    // method to sort queue
 
-    // Method to remove an key from queue.
-    public Node dequeue() {
-
-        // If queue is empty, return NULL.
-        if (this.front == null)
-            return null;
-
-        // Store previous front and move front one node ahead
-        Node temp = this.front;
-        this.front = this.front.getNext();
-
-
-        // If front becomes NULL, then change rear also as NULL
-        if (this.front == null)
-            this.rear = null;
-        return temp;
+    public void show() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
     }
 
+    public void peek() {
+        System.out.println(front.getData());
+    }
 
 }
