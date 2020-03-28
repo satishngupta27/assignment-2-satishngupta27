f4ef4c9fc0e5efc0fe11c2783931e08289c95a4f
SATISH <satish.gupta_cs18@gla.ac.in>
2020-03-28 05:46:35 -0700

    getter setter and constructor created

--- src/problem4/node/Node.java
+++ src/problem4/node/Node.java
@@ -6,4 +6,49 @@ public class Node {
     private TreeNode node;
 
     private Node next;
+
+
+    public Node(int data) {
+
+        node = new TreeNode(data);
+
+        next = null;
+
+    }
+
+
+    public Node(TreeNode treenode) {
+
+        node = treenode;
+
+        next = null;
+
+    }
+
+    public TreeNode getNode() {
+
+        return node;
+
+    }
+
+
+    public void setNode(TreeNode node) {
+
+        this.node = node;
+
+    }
+
+
+    public Node getNext() {
+
+        return next;
+
+    }
+
+
+    public void setNext(Node next) {
+
+        this.next = next;
+
+    }
 }
